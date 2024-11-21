7c5cedcd3a086f12cddda2e967ae8064adbdaf4e
adnguy3n <nguyeant@mail.gvsu.edu>
2024-11-21 01:27:07 -0500

    Turned the Navbar into its own component.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,140 +1,117 @@
 <script lang="ts">
-
+  import Navbar from "./Navbar.svelte";
 </script>
 
 <main>
-      <!-- Navbar -->
-      <header>
-        <nav class="navbar">
-          <a href="/" class="navbar-btn-1">Home</a>
-            <div class="nav-menu">
-              <div class="navbar-mobile-menu">
-                <span class="mobile-bar"></span>
-                <span class="mobile-bar"></span>
-                <span class="mobile-bar"></span>
-              </div>
-  
-              <ul>
-                <li class="navbar-btn-1">
-                  <a href="/" class="nav-link">Blog</a>
-                </li>
-                <li class="navbar-btn-1">
-                  <a href="/" class="nav-link">Contact</a>
-                </li>
-                <li class="navbar-btn-2">
-                  <a href="/" class="nav-link">⤓ Resume</a>
-                </li>
-              </ul>
-          </div>
-        </nav>
-      </header>
-  
-      <!-- Resume -->
-      <article class="wrapper">
-        <section>
-          <h1 class="resume-header">Anthony Nguyen</h1>
-        </section>
-  
-        <section class="resume-container">
-          <h2>Profile</h2>
-          <div class="resume-content">
-            <p>
-              Computer Science graduate and tech enthusiast with experience as a software developer. Proficient in Go, Python, and Typescript. Familiar with Linux environments. Experienced with Agile Software Development and working in Cross-Functional Teams. Supportive of open source development and enjoys participating in tech communities.
-            </p>
-          </div>
-        </section>
-  
-        <section class="resume-container">
-          <h2>Skills</h2>
-          <div class="resume-content">
-            <ul class="resume-skills">
-              <li>Go</li>
-              <li>Python</li>
-              <li>Typescript</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>Java</li>
-              <li>C</li>
-              <li>C++</li>
-              <li>C#</li>
-              <li>AWS</li>
-              <li>Git</li>
-              <li>Linux (Debian, Ubuntu)</li>
-            </ul>
-          </div>
-        </section>
-  
-        <section class="resume-container">
-          <h2>Experience</h2>
-          <div class="resume-content">
-            <div class="resume-experience">
-              <h3>Software Developer</h3>
-              <a href="https://www.configura.com/">Configura</a>
-              <br>Grand Rapids, MI
-              <br>June 2022 - June 2023
-              <ul>
-                <li>
-                  Developed extensions for Configura's Space Planning Software in a cross-functional team using Configura's proprietary programming language.
-                </li>
-                <li>
-                  Programmed virtual representations of office furniture, including their functionalities, to be placed into a 3D virtual environment.
-                </li>
-                <li>
-                  Collaborated with client companies on projects and maintained diverse code bases.
-                </li>
-              </ul>
-  
-              <h3>Intern</h3>
-              <a href="https://www.configura.com/">Configura</a>
-              <br>Grand Rapids, MI
-              <br>February 2022 - June 2022
-              <ul>
-                <li>
-                  Learned Configura's proprietary programming language, CM.
-                </li>
-                <li>
-                  Implemented a REST CRUD API in CM with .NET calls.
-                </li>
-              </ul>
-            </div>
-          </div>
-        </section>
-  
-        <section class="resume-container">
-          <h2>Projects</h2>
-          <div class="resume-content">
-            <div class ="resume-projects">
-            <h3>Cloud Resume Challenge</h3>
-              <ul>
-                <li>Technologies Used:</li>
-                <li><a href="https://github.com/adnguy3n/cloud-resume">Github Link</a></li>
-              </ul>
-              <h3>Pokemon Secret Base Editor</h3>
-              <ul>
-                <li>Technologies Used: Python</li>
-                <li><a href="https://github.com/adnguy3n/secret-base-editor">Github Link</a></li>
-              </ul>
-              <h3>AWS Virtual Table Top Server</h3>
-              <ul>
-                <li>Technologies Used: AWS EC2, S3, IAM</li>
-                <li><a href="https://github.com/adnguy3n">Github Link</a></li>
-              </ul>
-            </div>
-          </div>
-        </section>
-  
-        <section class="resume-container">
-          <h2>
-            Education
-          </h2>
-          <div class="resume-content">
-            <h3>Bachelor of Science in Computer Science</h3>
-            Grand Valley State University
-            <br>
-            Allendale, MI
-            <br>
-            Graduated April 2022
-          </div>
-        </section>
-      </article>
+  <!-- Navbar -->
+  <Navbar />
+
+  <!-- Resume -->
+  <article class="wrapper">
+    <section>
+      <h1 class="resume-header">Anthony Nguyen</h1>
+    </section>
+
+    <section class="resume-container">
+      <h2>Profile</h2>
+      <div class="resume-content">
+        <p>
+          Computer Science graduate and tech enthusiast with experience as a software developer. Proficient in Go, Python, and Typescript. Familiar with Linux environments. Experienced with Agile Software Development and working in Cross-Functional Teams. Supportive of open source development and enjoys participating in tech communities.
+        </p>
+      </div>
+    </section>
+
+    <section class="resume-container">
+      <h2>Skills</h2>
+      <div class="resume-content">
+        <ul class="resume-skills">
+          <li>Go</li>
+          <li>Python</li>
+          <li>Typescript</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Java</li>
+          <li>C</li>
+          <li>C++</li>
+          <li>C#</li>
+          <li>AWS</li>
+          <li>Git</li>
+          <li>Linux (Debian, Ubuntu)</li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="resume-container">
+      <h2>Experience</h2>
+      <div class="resume-content">
+        <div class="resume-experience">
+          <h3>Software Developer</h3>
+          <a href="https://www.configura.com/">Configura</a>
+          <br>Grand Rapids, MI
+          <br>June 2022 - June 2023
+          <ul>
+            <li>
+              Developed extensions for Configura's Space Planning Software in a cross-functional team using Configura's proprietary programming language.
+            </li>
+            <li>
+              Programmed virtual representations of office furniture, including their functionalities, to be placed into a 3D virtual environment.
+            </li>
+            <li>
+              Collaborated with client companies on projects and maintained diverse code bases.
+            </li>
+          </ul>
+
+          <h3>Intern</h3>
+          <a href="https://www.configura.com/">Configura</a>
+          <br>Grand Rapids, MI
+          <br>February 2022 - June 2022
+          <ul>
+            <li>
+              Learned Configura's proprietary programming language, CM.
+            </li>
+            <li>
+              Implemented a REST CRUD API in CM with .NET calls.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+
+    <section class="resume-container">
+      <h2>Projects</h2>
+      <div class="resume-content">
+        <div class ="resume-projects">
+        <h3>Cloud Resume Challenge</h3>
+          <ul>
+            <li>Technologies Used:</li>
+            <li><a href="https://github.com/adnguy3n/cloud-resume">Github Link</a></li>
+          </ul>
+          <h3>Pokemon Secret Base Editor</h3>
+          <ul>
+            <li>Technologies Used: Python</li>
+            <li><a href="https://github.com/adnguy3n/secret-base-editor">Github Link</a></li>
+          </ul>
+          <h3>AWS Virtual Table Top Server</h3>
+          <ul>
+            <li>Technologies Used: AWS EC2, S3, IAM</li>
+            <li><a href="https://github.com/adnguy3n">Github Link</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+
+    <section class="resume-container">
+      <h2>
+        Education
+      </h2>
+      <div class="resume-content">
+        <h3>Bachelor of Science in Computer Science</h3>
+        Grand Valley State University
+        <br>
+        Allendale, MI
+        <br>
+        Graduated April 2022
+      </div>
+    </section>
+  </article>
 </main>

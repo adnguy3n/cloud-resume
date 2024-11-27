568b068e0bbf414aba373ee497ee79fecdc3ce0c
adnguy3n <nguyeant@mail.gvsu.edu>
2024-11-27 03:18:09 -0500

    Added Sveltekit.

---
+++ frontend/src/routes/blog/+page.svelte
@@ -0,0 +1,2 @@
+<h1>blog</h1>
+<p>this is the blog page.</p>
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,17 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+    alias: {
+      $components: 'src/components',
+      $lib: 'src/lib',
+      $routes: 'src/routes',
+    }
+	}
+};
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess

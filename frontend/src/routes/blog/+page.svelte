c7064b84b311ebb22f4f1f5c33f71391bd97452d
adnguy3n <nguyeant@mail.gvsu.edu>
2024-12-12 16:05:09 -0500

    Added intro paragraph for the project and some css style unique to the page.

--- frontend/src/routes/blog/+page.svelte
+++ frontend/src/routes/blog/+page.svelte
@@ -1,2 +1,17 @@
-<h1>blog</h1>
-<p>this is the blog page.</p>
+<article>
+    <h1>Cloud Resume Challenge</h1>
+    <section>
+        <p>While looking for ideas for a personal project, the Cloud Resume Challenge is one I often come cross. While I have yet to acquire an cloud certification, I figured why not do the project first and just add the certification when I get it. After all, the important part is learning and I tend to learn better by simply doing something.</p>
+    </section>
+</article>
+
+<style>
+    article{
+        max-width: 40%;
+        margin: 0 auto;
+    }
+
+    section{
+        margin-top: 1rem;
+    }
+</style>

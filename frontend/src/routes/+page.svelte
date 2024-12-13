ff8587087b618e6ab3adc956991df5d625c2b38c
adnguy3n <nguyeant@mail.gvsu.edu>
2024-12-13 01:33:42 -0500

    Adjusted spacing in html for better readability.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -7,7 +7,10 @@
         <h2>Profile</h2>
         <div class="resume-content">
             <p>
-                Computer Science graduate and tech enthusiast with experience as a software developer. Proficient in Go, Python, and Typescript. Familiar with Linux environments. Experienced with Agile Software Development and working in Cross-Functional Teams. Supportive of open source development and enjoys participating in tech communities.
+                Computer Science graduate and tech enthusiast with experience as a software developer. 
+                Proficient in Go, Python, and Typescript. Familiar with Linux environments. 
+                Experienced with Agile Software Development and working in Cross-Functional Teams. 
+                Supportive of open source development and enjoys participating in tech communities.
             </p>
         </div>
     </section>
@@ -45,14 +48,16 @@
                 <br>June 2022 - June 2023
                 <ul>
                     <li>
-                        Developed extensions for Configura's Space Planning Software in a cross-functional team using Configura's proprietary programming language.
+                        Developed extensions for Configura's Space Planning Software in a cross-functional team using 
+                        Configura's proprietary programming language.
                     </li>
+
                     <li>
-                        Programmed virtual representations of office furniture, including their functionalities, to be placed into a 3D virtual environment.
+                        Programmed virtual representations of office furniture, including their functionalities, 
+                        to be placed into a 3D virtual environment.
                     </li>
-                    <li>
-                        Collaborated with client companies on projects and maintained diverse code bases.
-                    </li>
+
+                    <li>Collaborated with client companies on projects and maintained diverse code bases.</li>
                 </ul>
 
                 <h3>Intern</h3>
@@ -60,12 +65,9 @@
                 <br>Grand Rapids, MI
                 <br>February 2022 - June 2022
                 <ul>
-                    <li>
-                        Learned Configura's proprietary programming language, CM.
-                    </li>
-                    <li>
-                        Implemented a REST CRUD API in CM with .NET calls.
-                    </li>
+                    <li>Learned Configura's proprietary programming language, CM.</li>
+
+                    <li>Implemented a REST CRUD API in CM with .NET calls.</li>
                 </ul>
             </div>
         </div>
@@ -77,27 +79,55 @@
             <div class ="resume-projects">
                 <h3>Cloud Resume Challenge</h3>
                 <ul>
-                    <li>Technologies Used: Typescript, HTML, CSS, Svelte, AWS S3, Route 53, Lambda, Dynamo DB, API Gateway, Cloudfront, Certificate Manager</li>
+                    <li>
+                        Technologies Used: Typescript, HTML, CSS, Svelte, AWS S3, Route 53, Lambda, Dynamo DB, API Gateway, 
+                        Cloudfront, Certificate Manager
+                    </li>
+
                     <li><a href="https://github.com/adnguy3n/cloud-resume" target="_blank">Github Link</a></li>
-                    <li>Created an online portfolio hosted as a static website using AWS S3 Buckets. Keeps track of the number of visitors and updates the count stored in a database with lambda functions.</li>
+
+                    <li>
+                        Created an online portfolio hosted as a static website using AWS S3 Buckets. 
+                        Keeps track of the number of visitors and updates the count stored in a database with 
+                        lambda functions.
+                    </li>
                 </ul>
+
                 <h3>Pokemon Secret Base Editor</h3>
                 <ul>
                     <li>Technologies Used: Python</li>
+
                     <li><a href="https://github.com/adnguy3n/secret-base-editor" target="_blank">Github Link</a></li>
-                    <li>Forked an open source save editor for Pokemon Emerald Secret Bases, written in Python, and added functionality to edit Ruby and Sapphire save files, check if the user tries to load a Fire Red or Leaf Green save file, and fix garbage data in the save file.</li>
+
+                    <li>
+                        Forked an open source save editor for Pokemon Emerald Secret Bases, written in Python, 
+                        and added functionality to edit Ruby and Sapphire save files, check if the user tries to 
+                        load a Fire Red or Leaf Green save file, and fix garbage data in the save file.
+                    </li>
                 </ul>
+
                 <h3>AWS Virtual Table Top Server</h3>
                 <ul>
                     <li>Technologies Used: Linux, AWS EC2, S3, IAM</li>
+
                     <li><a href="https://github.com/adnguy3n/FoundryAWS_Server" target="_blank">Github Link</a></li>
-                    <li>Self-Hosted a virtual table top server for TTRPG's (e.g. Dungeons & Dragons) on an AWS EC2 Instance.</li>
+
+                    <li>
+                        Self-Hosted a virtual table top server for TTRPG's (e.g. Dungeons & Dragons) 
+                        on an AWS EC2 Instance.
+                    </li>
                 </ul>
+
                 <h3>Blog Website</h3>
                 <ul>
                     <li>Technologies Used: Go, Javascript, React, HTML, CSS, Tailwind, SQLite, Docker</li>
+
                     <li><a href="https://github.com/adnguy3n/Go-Blog-Website" target="_blank">Github Link</a></li>
-                    <li>Made a website that allows users to create, edit, and delete blog posts, and upload images to those posts using a Go backend and a React frontend.</li>
+
+                    <li>
+                        Made a website that allows users to create, edit, and delete blog posts, 
+                        and upload images to those posts using a Go backend and a React frontend.
+                    </li>
                 </ul>
             </div>
         </div>

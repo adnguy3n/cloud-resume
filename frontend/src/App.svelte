b9997089c6f956d62b9ce7bf3acc897bd26a05f9
adnguy3n <nguyeant@mail.gvsu.edu>
2025-05-07 20:01:48 -0400

    Updated resume, switched to pre-reversioned navbar, added footer.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import Navbar from "./Navbar.svelte";
+  import Navbar from "./lib/components/Navbar.svelte";
+  import Footer from "./lib/components/Footer.svelte";
 </script>
 
 <main>
@@ -7,111 +8,154 @@
   <Navbar />
 
   <!-- Resume -->
+  
   <article class="wrapper">
-    <section>
-      <h1 class="resume-header">Anthony Nguyen</h1>
-    </section>
+    <h1 class="resume-header">Anthony Nguyen</h1>
 
     <section class="resume-container">
-      <h2>Profile</h2>
-      <div class="resume-content">
-        <p>
-          Computer Science graduate and tech enthusiast with experience as a software developer. Proficient in Go, Python, and Typescript. Familiar with Linux environments. Experienced with Agile Software Development and working in Cross-Functional Teams. Supportive of open source development and enjoys participating in tech communities.
-        </p>
-      </div>
-    </section>
-
-    <section class="resume-container">
-      <h2>Skills</h2>
-      <div class="resume-content">
-        <ul class="resume-skills">
-          <li>Go</li>
-          <li>Python</li>
-          <li>Typescript</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Java</li>
-          <li>C</li>
-          <li>C++</li>
-          <li>C#</li>
-          <li>AWS</li>
-          <li>Git</li>
-          <li>Linux (Debian, Ubuntu)</li>
-        </ul>
-      </div>
-    </section>
-
-    <section class="resume-container">
-      <h2>Experience</h2>
-      <div class="resume-content">
-        <div class="resume-experience">
-          <h3>Software Developer</h3>
-          <a href="https://www.configura.com/">Configura</a>
-          <br>Grand Rapids, MI
-          <br>June 2022 - June 2023
-          <ul>
-            <li>
-              Developed extensions for Configura's Space Planning Software in a cross-functional team using Configura's proprietary programming language.
-            </li>
-            <li>
-              Programmed virtual representations of office furniture, including their functionalities, to be placed into a 3D virtual environment.
-            </li>
-            <li>
-              Collaborated with client companies on projects and maintained diverse code bases.
-            </li>
-          </ul>
-
-          <h3>Intern</h3>
-          <a href="https://www.configura.com/">Configura</a>
-          <br>Grand Rapids, MI
-          <br>February 2022 - June 2022
-          <ul>
-            <li>
-              Learned Configura's proprietary programming language, CM.
-            </li>
-            <li>
-              Implemented a REST CRUD API in CM with .NET calls.
-            </li>
-          </ul>
+        <h2>Profile</h2>
+        <div class="resume-content">
+            <p>
+                Computer Science graduate and tech enthusiast with experience as a software developer. 
+                Proficient in Go, Python, and Typescript. Familiar with Linux environments. 
+                Experienced with Agile Software Development and working in Cross-Functional Teams. 
+                Supportive of open source development and enjoys participating in tech communities.
+            </p>
         </div>
-      </div>
     </section>
 
     <section class="resume-container">
-      <h2>Projects</h2>
-      <div class="resume-content">
-        <div class ="resume-projects">
-        <h3>Cloud Resume Challenge</h3>
-          <ul>
-            <li>Technologies Used:</li>
-            <li><a href="https://github.com/adnguy3n/cloud-resume">Github Link</a></li>
-          </ul>
-          <h3>Pokemon Secret Base Editor</h3>
-          <ul>
-            <li>Technologies Used: Python</li>
-            <li><a href="https://github.com/adnguy3n/secret-base-editor">Github Link</a></li>
-          </ul>
-          <h3>AWS Virtual Table Top Server</h3>
-          <ul>
-            <li>Technologies Used: AWS EC2, S3, IAM</li>
-            <li><a href="https://github.com/adnguy3n">Github Link</a></li>
-          </ul>
+        <h2>Skills</h2>
+        <div class="resume-content">
+            <ul class="resume-skills">
+                <li>Go</li>
+                <li>Typescript</li>
+                <li>CSS</li>
+                <li>Git</li>
+                <li>AWS</li>
+                <li>C</li>
+                <li>C#</li>
+                <!-- Second Column-->
+                <li>Python</li>
+                <li>HTML</li>
+                <li>React</li>
+                <li>Svelte</li>
+                <li>Linux (Debian, Ubuntu)</li>
+                <li>C++</li>
+                <li>Java</li>
+            </ul>
         </div>
-      </div>
     </section>
 
     <section class="resume-container">
-      <h2>
+        <h2>Experience</h2>
+        <div class="resume-content">
+            <div class="resume-experience">
+                <h3>Software Developer</h3>
+                <a href="https://www.configura.com/" target="_blank">Configura</a>
+                <br>Grand Rapids, MI
+                <br>June 2022 - June 2023
+                <ul>
+                    <li>
+                        Developed extensions for Configura's Space Planning Software in a cross-functional team using 
+                        Configura's proprietary programming language.
+                    </li>
+
+                    <li>
+                        Programmed virtual representations of office furniture, including their functionalities, 
+                        to be placed into a 3D virtual environment.
+                    </li>
+
+                    <li>Collaborated with client companies on projects and maintained diverse code bases.</li>
+                </ul>
+
+                <h3>Intern</h3>
+                <a href="https://www.configura.com/" target="_blank">Configura</a>
+                <br>Grand Rapids, MI
+                <br>February 2022 - June 2022
+                <ul>
+                    <li>Learned Configura's proprietary programming language, CM.</li>
+
+                    <li>Implemented a REST CRUD API in CM with .NET calls.</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="resume-container">
+        <h2>Projects</h2>
+        <div class="resume-content">
+            <div class ="resume-projects">
+                <h3>Cloud Resume Challenge</h3>
+                <ul>
+                    <li>
+                        Technologies Used: Typescript, HTML, CSS, Svelte, AWS S3, Route 53, Lambda, Dynamo DB, API Gateway, 
+                        Cloudfront, Certificate Manager
+                    </li>
+
+                    <li><a href="https://github.com/adnguy3n/cloud-resume" target="_blank">Github Link</a></li>
+
+                    <li>
+                        Created an online portfolio hosted as a static website using AWS S3 Buckets. 
+                        Keeps track of the number of visitors and updates the count stored in a database with 
+                        lambda functions.
+                    </li>
+                </ul>
+
+                <h3>Pokemon Secret Base Editor</h3>
+                <ul>
+                    <li>Technologies Used: Python</li>
+
+                    <li><a href="https://github.com/adnguy3n/secret-base-editor" target="_blank">Github Link</a></li>
+
+                    <li>
+                        Forked an open source save editor for Pokemon Emerald Secret Bases, written in Python, 
+                        and added functionality to edit Ruby and Sapphire save files, check if the user tries to 
+                        load a Fire Red or Leaf Green save file, and fix garbage data in the save file.
+                    </li>
+                </ul>
+
+                <h3>AWS Virtual Table Top Server</h3>
+                <ul>
+                    <li>Technologies Used: Linux, AWS EC2, S3, IAM</li>
+
+                    <li><a href="https://github.com/adnguy3n/FoundryAWS_Server" target="_blank">Github Link</a></li>
+
+                    <li>
+                        Self-Hosted a virtual table top server for TTRPG's (e.g. Dungeons & Dragons) 
+                        on an AWS EC2 Instance.
+                    </li>
+                </ul>
+
+                <h3>Blog Website</h3>
+                <ul>
+                    <li>Technologies Used: Go, Javascript, React, HTML, CSS, Tailwind, SQLite, Docker</li>
+
+                    <li><a href="https://github.com/adnguy3n/Go-Blog-Website" target="_blank">Github Link</a></li>
+
+                    <li>
+                        Made a website that allows users to create, edit, and delete blog posts, 
+                        and upload images to those posts using a Go backend and a React frontend.
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="resume-container" style="border-bottom: 0px">
+    <h2>
         Education
-      </h2>
-      <div class="resume-content">
+    </h2>
+    <div class="resume-content">
         <h3>Bachelor of Science in Computer Science</h3>
         Grand Valley State University
         <br>
         Allendale, MI
         <br>
         Graduated April 2022
-      </div>
+    </div>
     </section>
   </article>
+
+  <Footer />
 </main>

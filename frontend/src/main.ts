ecc8517e4463e77e54b19a89c1e45bfe3347b127
adnguy3n <nguyeant@mail.gvsu.edu>
2025-05-07 20:05:24 -0400

    renamed App.svelte to Resume.svelte.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { mount } from 'svelte'
-import App from './App.svelte'
+import Resume from './Resume.svelte'
 
-const app = mount(App, {
+const app = mount(Resume, {
   target: document.getElementById('app')!,
 })
 
